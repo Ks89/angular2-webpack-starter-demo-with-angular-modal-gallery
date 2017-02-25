f50f77f273aad35b9e555c21a6f9ed65204706dc
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-02-25 02:03:14 +0100

    update to beta-next (after beta.1 but still unreleased)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,11 @@ import { AppState } from '../app.service';
 import { Title } from './title';
 import { XLargeDirective } from './x-large';
 
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
-import { Image, Action, ImageModalEvent } from 'angular-modal-gallery';
+import { Image, Action, ImageModalEvent, Description } from 'angular-modal-gallery';
 
 @Component({
   // The selector is what angular internally uses
@@ -38,31 +39,36 @@ export class HomeComponent implements OnInit, OnDestroy {
   public openModalWindowObservable: boolean = false;
   public imagePointerObservable: number = 0;
 
-  public imagesArray = [
+  public imagesArray: Image[] = [
     new Image(
-      '../../assets/images/gallery/thumbs/img1.jpg',
       '../../assets/images/gallery/img1.jpg',
-      'Image 1'
+      null, // no thumb
+      null, // no description
+      'http://www.google.com'
     ),
     new Image(
-      '../../assets/images/gallery/thumbs/img2.jpg',
-      '../../assets/images/gallery/img2.jpg',
-      'Image 2'
+      '../../assets/images/gallery/img2.png', // example with a PNG image
+      null, // no thumb
+      'Description 2',
+      null // url
     ),
     new Image(
-      '../../assets/images/gallery/thumbs/img3.jpg',
       '../../assets/images/gallery/img3.jpg',
-      'Image 3'
+      '../../assets/images/gallery/thumbs/img3.png', // example with a PNG thumb image
+      'Description 3',
+      'http://www.google.com'
     ),
     new Image(
-      '../../assets/images/gallery/thumbs/img4.jpg',
       '../../assets/images/gallery/img4.jpg',
-      'Image 4'
+      null, // no thumb
+      'Description 4',
+      'http://www.google.com'
     ),
     new Image(
-      '../../assets/images/gallery/thumbs/img5.jpg',
       '../../assets/images/gallery/img5.jpg',
-      'Image 5'
+      '../../assets/images/gallery/thumbs/img5.jpg',
+      null, // no description
+      null // url
     )
   ];
 
@@ -72,13 +78,36 @@ export class HomeComponent implements OnInit, OnDestroy {
   // array with a single image inside (the first one)
   public singleImage: Observable<Image[]> = Observable.of([
     new Image(
-      '../../assets/images/gallery/thumbs/img1.jpg',
       '../../assets/images/gallery/img1.jpg',
-      'Image 1'
+      '../../assets/images/gallery/thumbs/img1.jpg',
+      'Description 1',
+      'http://www.google.com'
     )]
   );
-  private subscription: Subscription;
 
+  // array of images initialized inside the onNgInit() of this component
+  // in an asynchronous way subscribing to an Observable with a delay.
+  // This is not a real use-case, but it's a way to simulate a scenario where
+  // you have to subscribe to an Observable to get data and to set public vars
+  public imagesArraySubscribed: Image[];
+
+  public customDescription: Description = {
+    imageText: 'Look this image ',
+    numberSeparator: ' of ',
+    beforeTextDescription: ' => '
+  };
+
+  public customFullDescription: Description = {
+    // you should build this value programmaticaly with the result of (show)="..()" event
+    customFullDescription: 'Custom description of the current visible image',
+    // if customFullDescription !== undefined, all other fields will be ignored
+    // imageText: '',
+    // numberSeparator: '',
+    // beforeTextDescription: '',
+  };
+
+  private subscription: Subscription;
+  private imagesArraySubscription: Subscription;
   // --------------------------------------------------------------------------------
   // TypeScript public modifiers
   constructor(
@@ -89,6 +118,9 @@ export class HomeComponent implements OnInit, OnDestroy {
   public ngOnInit() {
     console.log('hello `Home` component');
     // this.title.getData().subscribe(data => this.data = data);
+    this.imagesArraySubscription = Observable.of(null).delay(500).subscribe(() => {
+      this.imagesArraySubscribed = this.imagesArray;
+    });
   }
 
   public submitState(value: string) {
@@ -111,37 +143,44 @@ export class HomeComponent implements OnInit, OnDestroy {
     });
   }
 
-  public onImageLoaded(event: ImageModalEvent) {
+  onImageLoaded(event: ImageModalEvent) {
     // angular-modal-gallery will emit this event if it will load successfully input images
     console.log('onImageLoaded action: ' + Action[event.action]);
     console.log('onImageLoaded result:' + event.result);
   }
 
-  public onVisibleIndex(event: ImageModalEvent) {
+  onVisibleIndex(event: ImageModalEvent) {
+    this.customFullDescription.customFullDescription = `Custom description of visible image with index= ${event.result}`;
     console.log('action: ' + Action[event.action]);
     console.log('result:' + event.result);
   }
 
-  public onIsFirstImage(event: ImageModalEvent) {
+  onIsFirstImage(event: ImageModalEvent) {
     console.log('onfirst action: ' + Action[event.action]);
     console.log('onfirst result:' + event.result);
   }
 
-  public onIsLastImage(event: ImageModalEvent) {
+  onIsLastImage(event: ImageModalEvent) {
     console.log('onlast action: ' + Action[event.action]);
     console.log('onlast result:' + event.result);
   }
 
-  public onCloseImageModal(event: ImageModalEvent) {
+  onCloseImageModal(event: ImageModalEvent) {
     console.log('onClose action: ' + Action[event.action]);
     console.log('onClose result:' + event.result);
     this.openModalWindow = false;
   }
 
+  public addRandomImage() {
+    this.imagesArray.push(this.imagesArray[Math.floor(Math.random() * this.imagesArray.length)]);
+  }
   // release resources for example 5
   public ngOnDestroy() {
     if (this.subscription) {
       this.subscription.unsubscribe();
+    }
+    if (this.imagesArraySubscription) {
+      this.imagesArraySubscription.unsubscribe();
     }
   }
   // ----------------------
